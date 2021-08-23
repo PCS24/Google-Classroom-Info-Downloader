0ade783c6bab7f74417e34fb8039f66a46661c53
Naishadh Patel <65918272+MrNNP@users.noreply.github.com>
2021-08-23 15:37:02 -0400

    add jquery

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 //this file contains the code which is fetched by the bookmark and is run
 
-//test to make sure file is run 
+var jQueryScript = document.createElement('script');  
+jQueryScript.setAttribute('src','https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js');
+document.head.appendChild(jQueryScript);
+
+
 let studentData = $$('tr.qRU9Ec').filter(a=>!a.firstChild.innerText.includes('invited')).map(a=>{return{name:a.firstChild.innerText,student_id:a.lastChild.firstChild.firstChild.firstChild.attributes['aria-label'].value.toString().replace('Email','').replace('@edison.k12.nj.us','').trim()}})
 console.log(studentData);
