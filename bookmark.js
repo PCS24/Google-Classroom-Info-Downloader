af9ac1cbf204563df4c6a1b3799a24ffd2954d24
Naishadh Patel <65918272+MrNNP@users.noreply.github.com>
2021-08-23 12:25:09 -0400

    added console.log to script

---
+++ bookmark.js
@@ -0,0 +1,2 @@
+//this file contains the code that will be in the bookmark
+javascript:fetch('https://raw.githubusercontent.com/PCS24/Google-Classroom-Info-Downloader/main/script.js').then(res=>console.log(res));
