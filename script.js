4fc0cd1356176282ff58acc3d93fc1320e2cb688
Naishadh Patel <65918272+MrNNP@users.noreply.github.com>
2021-08-23 15:57:07 -0400

    test return

--- script.js
+++ script.js
@@ -3,3 +3,4 @@
 
 let studentData = Array.from(document.getElementsByClassName('qRU9Ec')).filter(a=>!a.firstChild.innerText.includes('invited')).map(a=>{return{name:a.firstChild.innerText,student_id:a.lastChild.firstChild.firstChild.firstChild.attributes['aria-label'].value.toString().replace('Email','').replace('@edison.k12.nj.us','').trim()}})
 console.log(studentData);
+return studentData;
