a7c16da370cfe74914263c4d7cd8971dff3de817
Naishadh Patel <65918272+MrNNP@users.noreply.github.com>
2021-08-23 15:40:00 -0400

    remove jquery

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 //this file contains the code which is fetched by the bookmark and is run
 
-var jQueryScript = document.createElement('script');  
-jQueryScript.setAttribute('src','https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js');
-document.head.appendChild(jQueryScript);
 
-
-let studentData = $$('tr.qRU9Ec').filter(a=>!a.firstChild.innerText.includes('invited')).map(a=>{return{name:a.firstChild.innerText,student_id:a.lastChild.firstChild.firstChild.firstChild.attributes['aria-label'].value.toString().replace('Email','').replace('@edison.k12.nj.us','').trim()}})
+let studentData = Array.from(document.getElementsByClassName('qRU9Ec')).filter(a=>!a.firstChild.innerText.includes('invited')).map(a=>{return{name:a.firstChild.innerText,student_id:a.lastChild.firstChild.firstChild.firstChild.attributes['aria-label'].value.toString().replace('Email','').replace('@edison.k12.nj.us','').trim()}})
 console.log(studentData);
