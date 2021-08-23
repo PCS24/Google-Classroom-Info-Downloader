12c5eca1cfdf80a9e36a078389b7b0e7bd9b26cf
Naishadh Patel <65918272+MrNNP@users.noreply.github.com>
2021-08-23 14:51:40 -0400

    studentData scraper +  update bookmark to use right branch

--- bookmark.js
+++ bookmark.js
@@ -1,6 +1,6 @@
 //this file contains the code that will be in the bookmark
 //javascript:
 (async function() {
-    fetch('https://raw.githubusercontent.com/PCS24/Google-Classroom-Info-Downloader/testing/is-code-running/script.js').then((res)=>res.text()).then(text=>eval(text));
+    fetch('https://raw.githubusercontent.com/PCS24/Google-Classroom-Info-Downloader/feat/scrape-names/script.js').then((res)=>res.text()).then(text=>eval(text));
   
 }());
--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 //this file contains the code which is fetched by the bookmark and is run
 
 //test to make sure file is run 
-console.log('file is being run')
+let studentData = $$('tr.qRU9Ec').filter(a=>!a.firstChild.innerText.includes('invited')).map(a=>{return{name:a.firstChild.innerText,student_id:a.lastChild.firstChild.firstChild.firstChild.attributes['aria-label'].value.toString().replace('Email','').replace('@edison.k12.nj.us','').trim()}})
+console.log(studentData);
