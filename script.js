1c06b3c8d628eda1cf6ab479efeee6a0235caf14
Naishadh Patel <65918272+MrNNP@users.noreply.github.com>
2021-08-23 16:39:39 -0400

    update teacher scraper

--- script.js
+++ script.js
@@ -11,5 +11,5 @@ try{
     console.log(e);
 }
 
-let teacherData = Array.from(document.getElementsByClassName('ycbm1d')).filter(a=>!a.firstChild.innerText.includes('invited')).map(a=>{return{name:a.firstChild.firstChild.lastChild.innerText,student_id:a.lastChild.lastChild.firstChild.firstChild.attributes['aria-label'].value.toString().replace('Email','').replace('@edison.k12.nj.us','').trim()}}).map((a,index)=>{if(index == 0){a.owner = true}else{a.owner=false}});
+let teacherData = Array.from(document.getElementsByClassName('ycbm1d')).filter(a=>!a.firstChild.innerText.includes('invited')).map(a=>{return{name:a.firstChild.firstChild.lastChild.innerText,student_id:a.lastChild.lastChild.firstChild.firstChild.attributes['aria-label'].value.toString().replace('Email','').replace('@edison.k12.nj.us','').trim()}}).map((a,index)=>{if(index == 0){a.owner = true}else{a.owner=false} return a});
 console.log(teacherData);
