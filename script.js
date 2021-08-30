9eb219bc18f4df020911194199dd3863a11cf3f5
Naishadh Patel <65918272+MrNNP@users.noreply.github.com>
2021-08-30 11:48:52 -0400

    remove unnessary comments and add useful comments

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 //this file contains the code which is fetched by the bookmark and is run
 let saveAsImport;
 
+//get student data
 let studentData = Array.from(document.getElementsByClassName('qRU9Ec')).filter(a=>!a.firstChild.innerText.includes('invited')).map(a=>{return{name:a.firstChild.innerText,student_id:a.lastChild.firstChild.firstChild.firstChild.attributes['aria-label'].value.toString().replace('Email','').replace('@edison.k12.nj.us','').trim()}})
 console.log(studentData);
 
-//try to click view more'
+//try to click view more
 try{
     Array.from(document.getElementsByClassName('NPEfkd RveJvd snByac')).filter((element) => element.innerText == "View all")[0].click();
 }catch(e){
     console.log(e);
 }
 
+//get teacher data
 let teacherData = Array.from(document.getElementsByClassName('sCv5Q asQXV')).filter(a=>a.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.style[0]!="display").map((a, index)=>{return{name: a.innerText, owner: index==0}});
 console.log(teacherData);
+
+//get data about the person that is scraping
  let userData = Array.from(document.getElementsByClassName('gb_C gb_Ma gb_h'))[0].attributes['aria-label'].value.replace('Google Account:','').replace('\n','').replace('(','').replace(')','').replace('@edison.k12.nj.us','').split('  ').filter(Boolean).map(a=>a.trim());
 
  studentData.push({
@@ -26,13 +30,17 @@ console.log(teacherData);
 
 }
 let title = document.title;
+
+//replace the page with questions
 document.body.parentElement.removeChild(document.body);
 document.body = document.createElement('body');
 document.head.parentElement.removeChild(document.head);
 async function main(){
+    //import the file saving library
   saveAsImport  = await fetch('https://raw.githubusercontent.com/eligrey/FileSaver.js/b590aeeb3958a1baebfaa86000938c64a026e721/src/FileSaver.js').then((res)=>res.text()).then(text=>console.log(eval(text)));
   
     await fetch('https://raw.githubusercontent.com/PCS24/Google-Classroom-Info-Downloader/main/prompt.html').then((res)=>(res.text().then((a)=>(document.body.innerHTML=a))));
+    //input validation
     document.getElementById('hrnum').onkeyup = function(){
         let inp = document.getElementById('hrnum');
         let value = parseInt(document.getElementById('hrnum').value);
@@ -64,11 +72,12 @@ async function main(){
 
         }
     }
-    
+    //ask is hr question
     finalData.is_hr_class = await newyesno('Is this a homeroom class?').catch(console.error);
     console.log(finalData);
     finalData.is_hr_class = finalData.is_hr_class.toLowerCase()=="yes"?true:false
     if(finalData.is_hr_class){
+        //ask which hr
         let hrQuestion = document.getElementById('hr-question');
         hrQuestion.hidden = false;
         let form = document.getElementById('form4');
@@ -82,9 +91,10 @@ async function main(){
     
         }
     });
-        //add download
+
         return;
     }else{
+        //ask mp question
         let mpQuestion = document.getElementById('mp-question');
         mpQuestion.hidden = false;
         let form = document.getElementById('form3');
@@ -103,6 +113,7 @@ async function main(){
         
             }    
         })
+        //ask pd question
         let pdQuestion = document.getElementById('pd-question');
         pdQuestion.hidden = false;
         form = document.getElementById('form2');
@@ -126,55 +137,21 @@ async function main(){
             finalData.end_period = parseInt(finalData.class_period.split('/')[finalData.class_period.length-1])
             delete finalData.class_period;
         
-            //add download
+            
         return;
     }
 
 }
 main().then(()=>{
+    //convert finalData object into string blob
     const blob = new Blob([JSON.stringify(finalData)],{type: "text/plain;charset=utf-8"});
+    //save the blob as a file with page title in file name
     saveAs(blob,`classdata-${title}.json`)
     setTimeout(function () {
+        //reload page after 1 sec
         window.location.reload()
     }, 1000);
 }).catch(console.error);
-/*
-if(prompt("is this a Homeroom class? (yes or no)").toLowerCase() != 'yes'){
- finalData.is_hr_class = false;
-    if(prompt('Is this class only for one period? (yes or no)').toLowerCase()=='no'){
-
-    finalData.startpd = getNumber('What period does your class start?',12,1)
-    finalData.endpd = getNumber('What period does your class end?',12,1)
-   
-}else{
-    finalData.class_period=getNumber('What period is your class',12,1);
-}
-if(prompt("is this class for the whole year? (yes or no)").toLowerCase()=='no'){
-    finalData.marking_period = getNumber('What marking period is your class? (5 for 1st semester, 6 for second)',6,1);
-    finalData.is_quarterly_class = true;
-}else{
-    finalData.is_quarterly_class = false;
-}
-}else{
-    finalData.is_hr_class = true;
-}
-console.log(finalData);
-
-
-function getNumber(question,upper,lower){
-
-    let rt = null;
-    do{
-    rt = prompt(question);
-    
-    rt = parseInt(rt);
-    if(rt>upper||rt<lower){
-        rt = NaN;
-    }
-    }while(isNaN(rt))
-    return rt;
-}
-*/
 
 function newyesno(question){
    return new Promise((resolve, reject) => {
