b8be36c018c5e4d5fa0213de1b5085363d895d8f
Naishadh Patel <65918272+MrNNP@users.noreply.github.com>
2021-08-29 22:40:57 -0400

    change bookmark to use main branch

--- bookmark.js
+++ bookmark.js
@@ -1,6 +1,6 @@
 //this file contains the code that will be in the bookmark
 //javascript:
 (function() {
-    fetch('https://raw.githubusercontent.com/PCS24/Google-Classroom-Info-Downloader/feat/scrape-names/script.js').then((res)=>res.text()).then(text=>console.log(eval(text)));
+    fetch('https://raw.githubusercontent.com/PCS24/Google-Classroom-Info-Downloader/main/script.js').then((res)=>res.text()).then(text=>console.log(eval(text)));
   
 }());
