5195c8b94ddd1761196e42856863c3ad1abe1eaf
Naishadh Patel <65918272+MrNNP@users.noreply.github.com>
2021-08-30 12:06:58 -0400

    remove /web folder and just use /root

--- bookmark.js
+++
@@ -1,4 +0,0 @@
-javascript:(function() {
-    fetch('https://raw.githubusercontent.com/PCS24/Google-Classroom-Info-Downloader/main/script.js').then((res)=>res.text()).then(text=>console.log(eval(text)));
-  
-}());
