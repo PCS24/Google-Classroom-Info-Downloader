39fb8ff70e88eecd128a6bba6882821d6b6a7142
Naishadh Patel <65918272+MrNNP@users.noreply.github.com>
2021-08-29 22:51:14 -0400

    remove comments from bookmark.js so that website can just fetch and insert into href tag

--- bookmark.js
+++ bookmark.js
@@ -1,6 +1,4 @@
-//this file contains the code that will be in the bookmark
-//javascript:
-(function() {
+javascript:(function() {
     fetch('https://raw.githubusercontent.com/PCS24/Google-Classroom-Info-Downloader/main/script.js').then((res)=>res.text()).then(text=>console.log(eval(text)));
   
 }());
