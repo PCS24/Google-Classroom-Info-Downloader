3dbaceaecd240f3799334691e0f83a14e90d8370
builderpepc <troy.hiruka@gmail.com>
2022-10-01 21:23:16 -0400

    fix student ID scraping

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 /* this file contains the code which is fetched by the bookmark and is run */
 let saveAsImport;
 
-let studentData = Array.from(document.getElementsByClassName('qRU9Ec')).filter(a=>!a.firstChild.innerText.includes('invited')).map(a=>{return{name:a.firstChild.innerText,student_id:parseInt(a.lastChild.firstChild.firstChild.firstChild.attributes['aria-label'].value.toString().replace('Email','').replace('@edison.k12.nj.us','').trim())}});
 /* get student data */
+let studentData = Array.from(document.getElementsByClassName('qRU9Ec')).filter(a=>!a.firstChild.innerText.includes('invited')).map(a=>{return{name:a.firstChild.innerText,student_id:parseInt(a.lastChild.firstChild.lastChild.innerText.toString().replace('Email','').replace('@edison.k12.nj.us','').trim())}});
 console.log(studentData);
 
 /* try to click view more */
